ad3b3ce30d926d50e72179347a46dffe28a366b4
apetrots <zachary.newsom1@gmail.com>
2021-10-22 13:45:13 -0700

    Trying to get KDEs for hurricane parameters

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import stats
 import math
+import matplotlib.pyplot as plt
 
 class Hurricane:
     def simulate(self, point):
@@ -15,10 +16,10 @@ class Hurricane:
         self.affected_radius = affected_radius # effectively the size of the hurricane, radius of area affected by hurricane in nautical miles (nm)
         self.path_points = path_points
 
-def calculate_path_points(position, distance_per_time_step, direction, time_step_count):
+def calculate_path_points(origin, distance_per_time_step, direction, time_step_count):
     points = []
     for i in range(time_step_count):
-        points.append(np.add(position, np.multiply(distance_per_time_step * i, direction)))
+        points.append(np.add(origin, np.multiply(distance_per_time_step * i, direction)))
     return points
 
 def normalized(vec):
@@ -28,23 +29,35 @@ def main():
     path_count = 20;
     time_step_count = 6
     time_step_duration = 2 # in hours
-    sample_size = 5 # equivalent to N_0 in paper
+    sample_size = 5 # equivalent to N_0 in paper, set to predetermined 7000, some method could be used to find optimal value, but outside of scope
+    test_data = [0, 0, 0.006, 0.001, 0.001, 0.002, 0.01, 0.004, 0.005, 0.004, 0.004, 0.004, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    affected_radius_kde = stats.gaussian_kde(test_data)
+    
+    test_x = np.linspace(0.0, 0.01)
+    plt.plot(test_x, affected_radius_kde.pdf(test_x))
+
+    plt.show()
+    print(affected_radius_kde.resample(path_count * time_step_count))
 
     # "Simulate N_0 hurricanes at landfall"
-    for hurr_idx in range(path_count):
-        path = calculate_path_points( [0.0, 0.0], 5.0, normalized([0.0, 1.0]), time_step_count) # in degrees
+    for scenario in range(sample_size):
+        path = calculate_path_points( [95.2, 28.9], 5.0, normalized([0.0, 1.0]), time_step_count) # latitude/longitude, in degrees, origin/landfall location is on coast of Texas
         # simulate hurricane
         hurricane = Hurricane(10.0, 2.0, 3.0, path) 
         landfall_eye_pos = hurricane.path_points[0]
-        pressure_diff = sqrt( (2.636 + 0.0394899 * landfall_eye_pos - np.log(hurricane.max_sustained_windspeed)) / 0.0005086 ) # in millibars
+        pressure_diff = np.sqrt( (2.636 + 0.0394899 * landfall_eye_pos[1] - np.log(hurricane.max_sustained_windspeed)) / 0.0005086 ) # in millibars
         holland_pressure_param = 1.38 + 0.00184 * pressure_diff - 0.00309*hurricane.max_sustained_windspeed
-        for scenario in range(sample_size):
-            land_decay_factor = np.random.lognormal(mean=-3.466, sigma=0.703, size=None) # "Take N_0 samples of a lognormal PDF with parameters to simulate the land decay factor"
-            for path in range(path_count):
-                for time_step in range(time_step_count):
-                    eye_pos = hurricane.path_points[time_step]
-                    new_pressure_diff = pressure_diff * math.exp(-land_decay_factor * (time_step * time_step_duration))
-                    new_radius_to_max_wind_speed = math.exp(2.636 - 0.0005086 * new_pressure_diff**2 + 0.0394899 * eye_pos[1])
+        land_decay_factor = np.random.lognormal(mean=-3.466, sigma=0.703, size=None) # "Take N_0 samples of a lognormal PDF with parameters to simulate the land decay factor"
+        for path in range(path_count):
+            for time_step in range(time_step_count):
+                eye_pos = hurricane.path_points[time_step]
+                #print(eye_pos)
+                new_pressure_diff = pressure_diff * math.exp(-land_decay_factor * (time_step * time_step_duration))
+                new_radius_to_max_wind_speed = math.exp(2.636 - 0.0005086 * new_pressure_diff**2 + 0.0394899 * eye_pos[1])
+                new_holland_pressure_param = 1.38 + 0.00184 * new_pressure_diff - 0.00309 * new_radius_to_max_wind_speed
+                new_max_sustained_windspeed = hurricane.max_sustained_windspeed * np.sqrt((new_holland_pressure_param * new_pressure_diff) / (holland_pressure_param * pressure_diff))
+                #new_hurricane.affected_radius = sample from affected_radius_kde
+
 
 
 
